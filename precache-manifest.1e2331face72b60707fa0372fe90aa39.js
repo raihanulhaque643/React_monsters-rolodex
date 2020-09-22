1fc892b5b9737935841d088a237b3a417a5b9c37
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-22 19:52:02 +0600

    Updates

---
+++ precache-manifest.1e2331face72b60707fa0372fe90aa39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3cec06901ca6e08630d459dab37c30",
+    "url": "/React_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7840c81afab9e4754b87",
+    "url": "/React_monsters-rolodex/static/css/main.e4ad67f4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React_monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7840c81afab9e4754b87",
+    "url": "/React_monsters-rolodex/static/js/main.7e25a9bd.chunk.js"
+  },
+  {
+    "revision": "adfd5c42ec678b653958",
+    "url": "/React_monsters-rolodex/static/js/runtime-main.d31b1d6a.js"
+  }
+]);
